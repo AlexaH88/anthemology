3b20aa49bb40aa9da7aa260f840721b932855a6c
Alexa Hendry <alexahendry88@gmail.com>
2022-11-15 17:06:12 +0000

    Set up base templates and static files

--- songs/views.py
+++ songs/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 
 def songs_test(request):
-    return HttpResponse("Songs Working!")
+    # return HttpResponse("Songs Working!")
+    return render(request, 'songs/song_list.html')
