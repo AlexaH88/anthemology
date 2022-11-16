3c4d6c1d9bb8b211ad8930b9c6ecb4f9debe4d10
Alexa Hendry <alexahendry88@gmail.com>
2022-11-16 15:55:20 +0000

    Set up signup, login, logout links

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+    path('signup/', views.signup, name="signup"),
+    path('login/', views.login, name="login"),
+    path('logout/', views.logout, name="logout"),
     path('profile/', views.profile_view, name="profile_view"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
 
+def signup(request):
+    return render(request, 'accounts/signup.html')
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
 def profile_view(request):
     return render(request, 'accounts/profile.html')
+
+
+def logout(request):
+    return render(request, 'accounts/logout.html')
