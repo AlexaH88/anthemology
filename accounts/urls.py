d8abe3634a9809421888551f3181df996580f0d4
Alexa Hendry <alexahendry88@gmail.com>
2022-11-16 16:52:31 +0000

    Create signup, login and logout views

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', views.signup, name="signup"),
-    path('login/', views.login, name="login"),
-    path('logout/', views.logout, name="logout"),
+    path('signup/', views.signup_view, name="signup_view"),
+    path('login/', views.login_view, name="login_view"),
+    path('logout/', views.logout_view, name="logout_view"),
     path('profile/', views.profile_view, name="profile_view"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 
-def signup(request):
-    return render(request, 'accounts/signup.html')
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # log the user in
+            login(request, user)
+            return redirect('accounts:profile_view')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
 
 
-def login(request):
-    return render(request, 'accounts/login.html')
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # log the user in
+            user = form.get_user()
+            login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect('accounts:profile_view')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
 
 
 def profile_view(request):
     return render(request, 'accounts/profile.html')
 
 
-def logout(request):
-    return render(request, 'accounts/logout.html')
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return render(request, 'accounts/logout.html')
