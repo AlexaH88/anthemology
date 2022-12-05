8aefda16569ae709e35476325cd17b226b61a68e
Alexa Hendry <alexahendry88@gmail.com>
2022-12-05 16:11:56 +0000

    Create song detail view, html, and url

--- songs/urls.py
+++ songs/urls.py
@@ -5,4 +5,5 @@ app_name = 'songs'
 
 urlpatterns = [
     path('', views.song_list, name="song_list"),
+    path('<slug:slug>/', views.song_detail, name="detail"),
 ]
--- songs/views.py
+++ songs/views.py
@@ -5,3 +5,8 @@ from .models import Song
 def song_list(request):
     songs = Song.objects.all().order_by('artist')
     return render(request, 'songs/song_list.html', {'songs': songs})
+
+
+def song_detail(request, slug):
+    song = Song.objects.get(slug=slug)
+    return render(request, 'songs/song_detail.html', {'song': song})
