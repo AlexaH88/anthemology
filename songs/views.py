4a4397de3634fd18199e89eee842b191f137f6ce
Alexa Hendry <alexahendry88@gmail.com>
2022-12-05 15:55:48 +0000

    Populate data in song list html and views

--- songs/views.py
+++ songs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Song
 
 
 def song_list(request):
-    return render(request, 'songs/song_list.html')
+    songs = Song.objects.all().order_by('artist')
+    return render(request, 'songs/song_list.html', {'songs': songs})
