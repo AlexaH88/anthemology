d59d42fba3c23b1728460dafac8b074eb809635f
Alexa Hendry <alexahendry88@gmail.com>
2022-12-05 15:37:31 +0000

    Create Song model and add default media image

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Song(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    album = models.CharField(max_length=100)
+    lyrics = models.TextField()
+    artwork = models.ImageField(default='default.png', blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
