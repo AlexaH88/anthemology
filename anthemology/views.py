cd3001367495dc651e298f36fd6558634b199db0
Alexa Hendry <alexahendry88@gmail.com>
2023-01-05 19:45:33 +0000

    Add searchbar functionality in nav bar

--- anthemology/views.py
+++ anthemology/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.db.models import Q
+from songs.models import Song
 
 
 def about(request):
     return render(request, 'about.html')
+
+
+def song_search(request):
+    if request.method == 'POST':
+        searched = request.POST['searched']
+        songs = Song.objects.filter(title__contains=searched)
+        return render(
+            request, 'song_search.html', {
+                'searched': searched,
+                'songs': songs
+                }
+            )
+    else:
+        return render(request, 'song_search.html')
