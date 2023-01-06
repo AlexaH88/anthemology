5c94135d60cde88917b9635d69c32220d7aba147
Alexa Hendry <alexahendry88@gmail.com>
2023-01-06 16:28:17 +0000

    Remove song jukebox page

--- songs/urls.py
+++ songs/urls.py
@@ -4,8 +4,7 @@ from . import views
 app_name = 'songs'
 
 urlpatterns = [
-    path('', views.song_jukebox, name="song_jukebox"),
-    path('song-list', views.song_list, name="song_list"),
+    path('', views.song_list, name="song_list"),
     path('<slug:slug>/', views.song_detail, name="detail"),
     path('my-songs', views.user_songs, name="user_songs"),
     path('add-song', views.add_song, name="add"),
