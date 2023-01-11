8ac51daa337fa0c0e2bd35b50c7f0c307d99a3db
Alexa Hendry <alexahendry88@gmail.com>
2023-01-11 17:18:12 +0000

    Add debug development variable to settings and env

--- anthemology/settings.py
+++ anthemology/settings.py
@@ -29,7 +29,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG ='DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ["anthemology.herokuapp.com", "localhost"]
 
