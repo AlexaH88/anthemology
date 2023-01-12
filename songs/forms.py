741017a38ecfd718add08bd25cfbdb6497b05762
Alexa Hendry <alexahendry88@gmail.com>
2023-01-12 21:34:03 +0000

    Remove artwork field from Song model and forms

--- songs/forms.py
+++ songs/forms.py
@@ -5,4 +5,4 @@ from . import models
 class SongForm(forms.ModelForm):
     class Meta:
         model = models.Song
-        fields = ['slug', 'title', 'artist', 'album', 'lyrics', 'artwork']
+        fields = ['slug', 'title', 'artist', 'album', 'lyrics']
